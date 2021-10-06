cd0870ab9652ea0b17e1cb3172cf736cf7cdf2ac
bucknermi <michaeliwbuckner@gmail.com>
2021-10-06 18:44:51 -0500

    Teller account menu

--- src/main/java/com/revature/controller/Menu.java
+++ src/main/java/com/revature/controller/Menu.java
@@ -3,5 +3,7 @@ package com.revature.controller;
 public interface Menu {
 	
 	void getMenu();
+	
+	
 
 }
---
+++ src/main/java/com/revature/controller/teller/TellerAccountEditMenu.java
@@ -0,0 +1,14 @@
+package com.revature.controller.teller;
+
+import com.revature.controller.Menu;
+
+public class TellerAccountEditMenu implements Menu {
+	
+	public void getMenu() {
+		
+		
+		
+	}
+	
+
+}
