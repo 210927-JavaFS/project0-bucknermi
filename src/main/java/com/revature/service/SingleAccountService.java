1ee4315aa2f6f7cd568a85fc36d4c66b2bde0e80
bucknermi <michaeliwbuckner@gmail.com>
2021-10-06 19:10:11 -0500

    Adding services class

---
+++ src/main/java/com/revature/service/SingleAccountService.java
@@ -0,0 +1,15 @@
+package com.revature.service;
+
+public class SingleAccountService {
+	
+	public double deposit(double x, double y) {
+		
+		return x+y;
+	}
+	
+	public double withdraw(double x, double y) {
+		
+		return x-y;
+	}
+
+}
