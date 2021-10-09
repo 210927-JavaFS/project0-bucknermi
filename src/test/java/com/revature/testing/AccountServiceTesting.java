f19aa6844d85f018a1cccd001608063602f66604
bucknermi <michaeliwbuckner@gmail.com>
2021-10-09 15:39:34 -0500

    beginning JUnit testing

--- src/test/java/com/revature/testing/AccountServiceTesting.java
+++ src/test/java/com/revature/testing/AccountServiceTesting.java
@@ -1,5 +1,37 @@
 package com.revature.testing;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.revature.controller.AccountController;
+import com.revature.controller.EntryMenu;
+import com.revature.model.Account;
+import com.revature.service.AccountService;
+
 public class AccountServiceTesting {
+	
+	AccountService as = new AccountService();
+	private static Logger log = LoggerFactory.getLogger(EntryMenu.class);
+	
+	@BeforeAll
+	public void testObjectCreator() {
+	as.createAccount("tester");
+	String s = as.toString();
+	log.info(s);
+	
+	}
+	
+	
+	@Test
+	public void testgetBalanceByID() {
+		
+		
+		
+	}
+	
 
 }
--- src/test/java/com/revature/testing/SingleAccountServiceTesting.java
+++ src/test/java/com/revature/testing/SingleAccountServiceTesting.java
@@ -1,5 +1,28 @@
 package com.revature.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.revature.service.SingleAccountService;
+
 public class SingleAccountServiceTesting {
+	
+	SingleAccountService sas = new SingleAccountService();
+	
+	int result;
+	
+	
+	@Test
+	public void testDeposit() {
+		result = sas.deposit(12, 13);
+		assertEquals(25, result);
+	}
+	
+	@Test public void testWithdraw() {
+		result = sas.withdraw(25, 12);
+		assertEquals(13, result);
+	}
+	
 
 }
