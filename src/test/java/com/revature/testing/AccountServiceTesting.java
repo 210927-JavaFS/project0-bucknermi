37d9e013d1d22a6eead643d1130564bae1b34af5
bucknermi <michaeliwbuckner@gmail.com>
2021-10-09 16:38:21 -0500

    JUnit Testing

--- src/test/java/com/revature/testing/AccountServiceTesting.java
+++ src/test/java/com/revature/testing/AccountServiceTesting.java
@@ -1,5 +1,9 @@
 package com.revature.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -7,31 +11,69 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.revature.controller.AccountController;
-import com.revature.controller.EntryMenu;
-import com.revature.model.Account;
 import com.revature.service.AccountService;
 
 public class AccountServiceTesting {
+
+	int result;
+	int testcase;
+	boolean result1;
+	int i = 15;
 	
-	AccountService as = new AccountService();
-	private static Logger log = LoggerFactory.getLogger(EntryMenu.class);
-	
-	@BeforeAll
-	public void testObjectCreator() {
-	as.createAccount("tester");
-	String s = as.toString();
-	log.info(s);
-	
-	}
-	
-	
-	@Test
-	public void testgetBalanceByID() {
-		
-		
-		
-	}
 	
 
+	AccountService as = new AccountService();
+	private static Logger log = LoggerFactory.getLogger(AccountServiceTesting.class);
+	
+	@BeforeAll
+	public int testAccountCreator() {
+		as.newLogin("test", "test");
+		as.createAccount("test");
+		log.info("A test account has been successfully created to be used in the JUnit tests");
+		
+	}
+
+	@Test
+	public void getBalanceByIDTester() {
+		result = as.getBalanceID(1);
+		assertEquals(1000, result);
+
+	}
+
+	@Test
+	public void getExistByIDTest() {
+		result1 = as.getExistByID(1);
+		assertTrue(result1);
+	}
+
+	@Test
+	public void makeDepositByIDTest() {
+		testcase = as.getBalanceID(1) + i;
+		result = as.makeDepositByID(1, i);
+
+		assertEquals(testcase, result);
+	}
+
+	@Test
+	public void makeWithdrawByIDTest() {
+		testcase = as.getBalanceID(1) - i;
+		result = as.makeWithdrawByID(1, i);
+
+		assertEquals(testcase, result);
+	}
+
+	@Test
+	public void checkIDByUsernameTest() {
+		result1 = as.checkIDByUsername(1, "cname1");
+		assertTrue(result1);
+	}
+
+	@Test
+	public void newUsernameTest() {
+		result1 = as.newUsername("cname1");
+		assertFalse(result1);
+	}
+
+	@Test
+	
 }
