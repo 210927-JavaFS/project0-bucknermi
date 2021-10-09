8f6739bcab47633a0defc188df3b62dc3b0eaa40
bucknermi <michaeliwbuckner@gmail.com>
2021-10-09 17:42:29 -0500

    Login service JUnit testing

--- src/test/java/com/revature/testing/LoginServiceTesting.java
+++ src/test/java/com/revature/testing/LoginServiceTesting.java
@@ -1,7 +1,52 @@
 package com.revature.testing;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.revature.service.CustomerLoginService;
+import com.revature.service.ManagerLoginService;
+import com.revature.service.TellerLoginService;
+
 public class LoginServiceTesting {
 	
+	String username;
+	String password;
+	boolean result;
+	private static Logger log = LoggerFactory.getLogger(SingleAccountServiceTesting.class);
+	CustomerLoginService cls = new CustomerLoginService();
+	TellerLoginService tls = new TellerLoginService();
+	ManagerLoginService mls = new ManagerLoginService();
 	
+	@Test
+	public void CustomerLoginTest() {
+		log.info("Testing );
+		username = "test";
+		password = "test";
+		result = cls.testLogin(username, password);
+		assertTrue(result);
+		
+	}
+	
+	@Test
+	public void TellerLoginTest() {
+		username = "tname1";
+		password = "tpass1";
+		result = tls.testLogin(username, password);
+		assertTrue(result);
+		
+	}
+	
+	@Test
+	public void ManagerLoginTest() {
+		username = "test";
+		password = "test";
+		result = mls.testLogin(username, password);
+		assertFalse(result);
+		
+	}
 
 }
